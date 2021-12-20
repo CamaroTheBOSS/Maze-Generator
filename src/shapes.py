31b9d42f6057eb36724b5b6eb8c3c749ca7a2de4
Kacper Plesiak <plesiakmail@gmail.com>
2021-12-21 00:28:08 +0100

    functions which creates grid of given shape were updated to v2

--- src/shapes.py
+++ src/shapes.py
@@ -1,7 +1,37 @@
 from graph import Graph
 
 
-# creates one hexagon
+def BuildShape(G: Graph, x, y, shiftX, shiftY, v=6):
+    # 3. Adding Nodes
+    Added = []
+    if (x, y) not in G.nodes.values():
+        G.addNode(x, y)
+    Added.append((x, y))
+    for k in range(v - 1):
+        x += shiftX[k]
+        y += shiftY[k]
+        if (x, y) not in G.nodes.values():
+            G.addNode(x, y)
+        Added.append((x, y))
+
+    # 4. Adding Edges and Faces
+    Face = []
+    for i in range(len(Added)):
+        value1 = Added[i - 1]
+        value2 = Added[i]
+
+        idx1 = list(G.nodes.keys())[list(G.nodes.values()).index(value1)]
+        idx2 = list(G.nodes.keys())[list(G.nodes.values()).index(value2)]
+
+        if idx2 not in G.edges[idx1]:
+            G.edges[idx1].append(idx2)
+        if idx1 not in G.edges[idx2]:
+            G.edges[idx2].append(idx1)
+
+        Face.append(idx2)
+    G.faces.append(Face)
+
+
 def Hex(G: Graph, column, row, a):
     # 1. Defining starting point
     if (row + 1) % 2 == 0:
@@ -13,19 +43,8 @@ def Hex(G: Graph, column, row, a):
     # 2. Defining Hex shape
     shiftX = [a, 0, -a, -a, 0, a]
     shiftY = [0.5*a, a, 0.5*a, -0.5*a, -a, -0.5*a]
-    Face = []
 
-    # 3. Adding Nodes and Edges
-    G.addNode(x, y)
-    for k in range(5):
-        x += shiftX[k]
-        y += shiftY[k]
-        G.addNode(x, y)
-        G.addEdge(len(G.nodes) - 1, len(G.nodes) - 2)
-        Face.append(G.edges[-1])
-    G.addEdge(len(G.nodes) - 1, len(G.nodes) - 6)
-    Face.append(G.edges[-1])
-    G.addFace(Face)
+    BuildShape(G, x, y, shiftX, shiftY, v=6)
 
 
 def Triangle(G: Graph, column, row, a):
@@ -52,18 +71,7 @@ def Triangle(G: Graph, column, row, a):
         shiftX = [a, -0.5*a, -0.5*a]
         shiftY = [0, h, -h]
 
-    # 2. Adding Nodes and Edges
-    Face = []
-    G.addNode(x, y)
-    for k in range(2):
-        x += shiftX[k]
-        y += shiftY[k]
-        G.addNode(x, y)
-        G.addEdge(len(G.nodes) - 1, len(G.nodes) - 2)
-        Face.append(G.edges[-1])
-    G.addEdge(len(G.nodes) - 1, len(G.nodes) - 3)
-    Face.append(G.edges[-1])
-    G.addFace(Face)
+    BuildShape(G, x, y, shiftX, shiftY, v=3)
 
 
 def Square(G: Graph, column, row, a):
@@ -73,14 +81,4 @@ def Square(G: Graph, column, row, a):
     shiftX = [a, 0, -a, 0]
     shiftY = [0, -a, 0, a]
 
-    Face = []
-    G.addNode(x, y)
-    for k in range(3):
-        x += shiftX[k]
-        y += shiftY[k]
-        G.addNode(x, y)
-        G.addEdge(len(G.nodes) - 1, len(G.nodes) - 2)
-        Face.append(G.edges[-1])
-    G.addEdge(len(G.nodes) - 1, len(G.nodes) - 4)
-    Face.append(G.edges[-1])
-    G.addFace(Face)
+    BuildShape(G, x, y, shiftX, shiftY, v=4)
