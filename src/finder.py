8e917388ccfe33ce30796dcab1ef01d758f41e69
Kacper Plesiak <plesiakmail@gmail.com>
2021-12-21 00:34:02 +0100

    BFS were updated to v2

--- src/finder.py
+++ src/finder.py
@@ -3,48 +3,43 @@ from graph import *
 
 # BreadthFirstSearch algorithm does not make us 100% sure that the found path is the shortest one
 def BreadthFirstSearch(F: Graph, startnode: int, goalnode: int):
+    # 1. Choosing random cell
     idx = startnode
     Visited = [idx]
     Backtracking = [idx]  # contains information about the solution
 
-    # 1. Repeat while current node is not goalnode
+    # 2. While any node is unvisited
     while idx != goalnode:
-        # 1.1 determining possible ways (to unvisited neighbours)
+        # 2.1 determining possible ways (to unvisited neighbours)
         ways = []
-        for edge in F.edges:
-            if edge[0] == idx or edge[1] == idx:
-                if edge[0] in Visited and edge[1] in Visited:
-                    pass
-                else:
-                    ways.append(edge)
-        # 1.2 choose the first one
+        for e in F.edges[idx]:
+            if e in Visited and idx in Visited:
+                pass
+            else:
+                ways.append((idx, e))
+
+        # 2.2 choose first way
         if len(ways) > 0:
             way = ways[0]
-            ways.remove(way)
         else:  # dead end
-            if len(Backtracking) > 1:
+            if len(Backtracking) > 0:
                 idx = Backtracking[-2]
                 del Backtracking[-1]
                 continue
             else:
                 break
-        # 1.3 to Visited list add next node which way leads to and add this node to backtracking list
-        if way[0] == idx:
-            idx = way[1]
-        else:
-            idx = way[0]
 
-        if way[0] == idx:
-            Visited.append(way[0])
-            Backtracking.append(way[0])
-        else:
-            Visited.append(way[1])
-            Backtracking.append(way[1])
+        # 2.3 to Visited list add next node which way leads to and add this node to backtracking list
+        idx = way[1]
+        Visited.append(idx)
+        Backtracking.append(idx)
 
-    # 2. Connect nodes from Backtracking list and return Solution
-    Solution = []
-    for idx, element in enumerate(Backtracking):
-        if idx != 0:
-            Solution.append((Backtracking[idx - 1], Backtracking[idx]))
+    Solution = {}
+    for i in range(len(F.edges)):
+        Solution[i] = []
 
+    for i in range(len(Backtracking)):
+        if i > 0:
+            Solution[Backtracking[i - 1]].append(Backtracking[i])
+            Solution[Backtracking[i]].append(Backtracking[i - 1])
     return Solution
