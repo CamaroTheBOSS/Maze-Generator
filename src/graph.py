1c5a76392d718a21425e63e72f4cc3b09bce71e6
Kacper Plesiak <plesiakmail@gmail.com>
2021-12-21 00:26:57 +0100

    graph func were updated to v2

--- src/graph.py
+++ src/graph.py
@@ -3,24 +3,34 @@ import matplotlib.pyplot as plt
 
 class Graph:
     def __init__(self):
-        self.nodes = []
-        self.edges = []
+        self.nodes = {}
+        self.edges = {}
         self.faces = []
 
     def addNode(self, x, y):  # takes coordinates
-        self.nodes.append((x, y))
+        self.nodes[len(self.nodes)] = (x, y)
+        self.edges[len(self.nodes) - 1] = []
 
     def addEdge(self, nidx1: int, nidx2: int):  # takes nodes indexes
         if len(self.nodes) > nidx1 and len(self.nodes) > nidx2:
-            self.edges.append((nidx1, nidx2))
+            self.edges[nidx1].append(nidx2)
 
-    def addFace(self, edges: list):
-        self.faces.append(edges)
+    def addFace(self, nodes):
+        self.faces.append(nodes)
 
-    def plotEdge(self, k, color='b'):
-        nidx1 = self.edges[k][0]
-        nidx2 = self.edges[k][1]
-        x = [self.nodes[nidx1][0], self.nodes[nidx2][0]]
-        y = [self.nodes[nidx1][1], self.nodes[nidx2][1]]
-
-        plt.plot(x, y, color)
+    def plotGraph(self, color='b'):
+        # 1. to not duplicated the edges and improve optimization if node
+        Added = []
+        for k in self.nodes:
+            x, y = [], []
+            if k in Added:
+                continue
+            for node in self.edges[k]:
+                if node in Added:
+                    continue
+                x.append(self.nodes[k][0])
+                y.append(self.nodes[k][1])
+                x.append(self.nodes[node][0])
+                y.append(self.nodes[node][1])
+            Added.append(k)
+            plt.plot(x, y, color)
